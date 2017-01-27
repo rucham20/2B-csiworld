1832854f5913b59f384e514b34ee373393dec104
Sunnie Li <mac6@129mac18.valleychristian.net>
2017-01-27 10:59:24 -0800

    Whole page can change

--- webapp.js
+++ webapp.js
@@ -2,8 +2,8 @@
 
 //"Next" button
 function next() {
-	{
 	document.getElementById("question").innerHTML= "This is the next page";
+	document.getElementById("story").innerHTML = "This is the storyline";
+	document.getElementById("CSI").innerHTML = "This is also the storyline";
 	document.getElementById("image").setAttribute("src", "images/test.jpg");
-	}
 }
