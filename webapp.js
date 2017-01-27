6d18b37e104c739c76395751131fb846c85f563e
Sunnie Li <mac6@129mac18.valleychristian.net>
2017-01-27 10:48:09 -0800

    Can reload text

--- webapp.js
+++ webapp.js
@@ -2,8 +2,8 @@
 
 //"Next" button
 function next() {
-
-document.getElementById("question").innerHTML;
-
-document.getElementById("image").setAttribute("src", "images/test.jpg");
+	{
+	document.getElementById("question").innerHTML= "This is the next page";
+	document.getElementById("image").setAttribute("src", "images/test.jpg");
+	}
 }
