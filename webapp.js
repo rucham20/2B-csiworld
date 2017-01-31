effbdd91ebfb87efaa1cddd0dfbc41fd83e6bef2
Sunnie Li <mac6@129mac18.valleychristian.net>
2017-01-31 11:06:45 -0800

    made first two pages

--- webapp.js
+++ webapp.js
@@ -2,8 +2,9 @@
 
 //"Next" button
 function next() {
-	document.getElementById("title").innerHTML= "Hello there! I am the main character in this story";
+	var name = prompt("What do you want your main character's name to be")
+	document.getElementById("title").innerHTML= "Hello there! I am " + name;
 	document.getElementById("stories").innerHTML = "Welcome to CSI world";
-	document.getElementById("remind").innerHTML = "Do you want to continue?";
+	document.getElementById("remind").innerHTML = "Do you want to continue?"; 
 	document.getElementById("image").setAttribute("src", "images/dectective.png");
 }
