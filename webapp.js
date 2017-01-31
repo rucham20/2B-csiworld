0545c8e09d696dfa8da078158b0b732a05038925
Sunnie Li <mac6@129mac18.valleychristian.net>
2017-01-31 11:25:23 -0800

    Attempting switch

--- webapp.js
+++ webapp.js
@@ -1,10 +1,10 @@
 // JavaScript Document
 
 //"Next" button
-function next() {
+function next(choice) {
 	var name = prompt("What do you want your main character's name to be")
 	document.getElementById("title").innerHTML= "Hello there! I am " + name;
 	document.getElementById("stories").innerHTML = "Welcome to CSI world";
 	document.getElementById("remind").innerHTML = "Do you want to continue?"; 
-	document.getElementById("image").setAttribute("src", "images/dectective.png");
-}
+	document.getElementById("image").setAttribute("src", "images/dectective.png")
+	}
