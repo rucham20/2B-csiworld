582635d257cd915179cfb291a2c352d6d0c98c70
Sunnie Li <mac6@129mac18.valleychristian.net>
2017-02-17 11:08:40 -0800

    Merge remote-tracking branch 'origin/Javascript' into Javascript
    # Conflicts:
    #	webapp.js

--- webapp.js
+++ webapp.js
@@ -31,21 +31,21 @@ function next(x) {
 		document.getElementById("storyline").innerHTML= "You call your colleagues to make sure of what you're seeing. Dectective Joe picks up the phone and you describe what you saw and your location. There is a long pause, then he tells you to come to the office for further investigation.";
 		document.getElementById("remind").innerHTML = "Do you want to go to the office or get some rest?";
 		document.getElementById("buttons").innerHTML = "<input type='button' onClick='next(8)' value='Go to the office'> <input type='button' onClick='next( )' value= 'Get some rest and go tomorrow'>";
-		document.getElementById("image").setAttribute("src", "images/purse.jpg");
+		document.getElementById("image").innerHTML="<img src='images/joe.png' alt='test' width='455'>";
 		break;
 	case 5: 
 	document.getElementById("title").innerHTML = "The Finding of the Fingerprints";
 	document.getElementById("storyline").innerHTML= "You go back to your apartment and immediately fall asleep. The next day, you wake up and go to your office to tell your colleagues and boss, Dectective Patty, what you saw last night. You then remember the gun you had kept in your purse and give it to the boss. You hand the gun over, asking her to dust for fingerprints of the shooter. She is delighted to know that you have the most valuable evidenc in your possession. She explains that the fingerprint will lead right to the killer.";
 		document.getElementById("remind").innerHTML = "Do you want to help Patty?";
 		document.getElementById("buttons").innerHTML = "<input type='button' onClick='next(6)' value='Yes!'> <input type='button' onClick='next(6)' value='No... You have other things to do.'>";
-		document.getElementById("image").setAttribute("src", "images/fingerprint.jpg");
+		document.getElementById("image").innerHTML="<img src='images/fingerprint.jpg' alt='test' width='455'>";
 		break;
 	case 6:
 	document.getElementById("title").innerHTML = "Victory for All... Except for Regina";
 	document.getElementById("storyline").innerHTML = "As you search for the files, Patty rushes in exclaiming that she found the killer, thanks to you. She says that the killer is none other than Hayley Smith's celebrity enemy, Regina Wright!! Patty says that she sent the police to her location to put her behind bars right away.";
 	document.getElementById("remind").innerHTML = "Let's celebrate!!!";
 	document.getElementById("buttons").innerHTML = "<input type='button' onClick='next(7)' value='Dance alllll night'>";
-	document.getElementById("image").setAttribute("src", "images/jail.jpg");
+	document.getElementById("image").innerHTML="<img src='images/jail.jpg' alt='test' width='455'>";
 	break;
 	
 	case 7:
@@ -53,7 +53,7 @@ function next(x) {
 	document.getElementById("storyline").innerHTML = "That night, you, Patty and the rest of the detectives from your team go to a restaurant to celebrate your success. While you are eating, you look at the TV and find Regina's picture all over the screen. Next to that you find your picture with a title of 'Our Famous Dectective!' Right then Patty gets everyone's attention and announces that you are promoted to head dectective! You are thrilled by this news. Many successful years later, you become a world famous dectective and live to your dreams.";
 	document.getElementById("remind").innerHTML = "Do you want to restart the game and get a different ending?";
 	document.getElementById("buttons").innerHTML = "<input type='button' onClick='next(1)' value='Restart the story'>";
-	document.getElementById("image").setAttribute("src", "images/trophy.png");
+	document.getElementById("image").innerHTML="<img src='images/trophy.png' alt='test' width='455'>";
 	break;
 	
 	case 8:
@@ -61,14 +61,14 @@ function next(x) {
 	document.getElementById("storyline").innerHTML= "You go to your office and meet one of your colleagues, Dectective Joe. You tell him what you had seen. You also mention Hayley Smith's dead body (the one you saw on the road). He asks you if you have any evidence, but you remember that you left it behind. He says that it might be hard to solve the case since there is no evidence, but he says that there may be some footage on the security cameras. He also says to tell your boss, Dectective Patty, as soon as you find the killer. You tell him the location and find the footage. You saw a streak of red hair from the black figure that is holding a gun and running away.";
 		document.getElementById("remind").innerHTML = "Do you want to investigate further with the footage, or search for other evidence?";
 		document.getElementById("buttons").innerHTML = "<input type='button' onClick='next(9)' value='Investigate'> <input type='button' onClick='next(9)' value='Search for evidence'>";
-		document.getElementById("image").setAttribute("src", "images/shadow.jpg");
+		document.getElementById("image").innerHTML="<img src='images/shadow.jpg' alt='test' width='455'>";
 		break;
 	case 9:
 	document.getElementById("title").innerHTML= "Suspects Found";	
-	document.getElementById("storyline").innerHTML= "You suggest that th shooter would be Hayley Smith's enemies or competition. Joe agrees and pulls out files and searches the internet for Hayley's enemies with red hair. He finds a list of people that match the description. There are three match:";
-		document.getElementById("remind").innerHTML = "Who do tou want to investigate?";
+	document.getElementById("storyline").innerHTML= "You suggest that the shooter would be Hayley Smith's enemies or competition. Joe agrees and pulls out files and searches the internet for Hayley's enemies with red hair. He finds a list of people that match the description. There are three matches:";
+		document.getElementById("remind").innerHTML = "Who do you want to investigate?";
 		document.getElementById("buttons").innerHTML = "<input type='button' onClick='next(10)' value='Susan Johnson'> <input type='button' onClick='next(11)' value='Katie Rose'> <input type='button' onClick= 'next(12)' value='Regina Wright'>";
-		document.getElementById("image").setAttribute("src", "images/susanJohnson.jpg");
+		document.getElementById("image").innerHTML="<img src='images/susanJohnson.jpg' alt='test' width='455'><img src='images/katieRose.jpg' alt='test' width='455'><img src='images/reginaWright.jpg' alt='test' width='455'>"; 
 		break;
 		}
 	}
