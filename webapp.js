ee6008452c6bee9a298b3df5ca97d8838318c12b
Sunnie Li <mac6@129mac18.valleychristian.net>
2017-03-08 11:38:14 -0800

    name works

--- webapp.js
+++ webapp.js
@@ -1,17 +1,15 @@
 // JavaScript Document
-
+var name = "";
 //"Next" button
 function next(x) {
 	switch (x) {
 	case 1:
-		var name = prompt("What do you want your main character's name to be");
+		name = prompt("What do you want your main character's name to be?");
 		document.getElementById("title").innerHTML= "The Night At Central Park";
 		document.getElementById("storyline").innerHTML= "You are walking back from your dectective office to your apartment. You hear a scream coming from Central Park. After that, follows a sound of a gunshot. You go toward the road and finds a body covered with blood and see a shadow running in the distance with a shiny black gun, a model rare to find.";
 		document.getElementById("remind").innerHTML = "Do you want to walk toward the body or call your dectective colleagues?";
 		document.getElementById("buttons").innerHTML = "<input type='button' onClick='next(2)' value='Walk towards the body'> <input type='button'  onClick='next(4)' value='Call your colleagues'>";
-
-		document.getElementById("image").innerHTML= "<img src='images/shadow1.png' alt= 'test' width= '455'>";
-		document.getElementById("image").innerHTML = "<img src='images/shadow1.jpg'>";
+		document.getElementById("image").innerHTML="<img src='images/shadow1.jpg' alt='test' width='455'>";
 		break;
 		
 	case 2:
@@ -71,7 +69,7 @@ function next(x) {
 	document.getElementById("storyline").innerHTML= "You suggest that the shooter would be Hayley Smith's enemies or competition. Joe agrees and pulls out files and searches the internet for Hayley's enemies with red hair. He finds a list of people that match the description. There are three matches:";
 		document.getElementById("remind").innerHTML = "Who do you want to investigate?";
 		document.getElementById("buttons").innerHTML = "<input type='button' onClick='next(10)' value='Susan Johnson'> <input type='button' onClick='next(11)' value='Katie Rose'> <input type='button' onClick= 'next(12)' value='Regina Wright'>";
-		document.getElementById("image").innerHTML="<img src='images/susanJohnson.jpg' alt='test' width='455'><img src='images/katieRose.jpg' alt='test' width='455'><img src='images/reginaWright.jpg' alt='test' width='455'>"; 
+		document.getElementById("image").innerHTML="<img src='images/susanJohnson.jpg' alt='test' width='300'><img src='images/katieRose.jpg' alt='test' width='300'><img src='images/reginaWright.jpg' alt='test' width='300'>"; 
 		break;
 	case 10:
 	document.getElementById("title").innerHTML = "Susan";
@@ -112,8 +110,8 @@ case 15:
 	document.getElementById("title").innerHTML = "Consequence";
 	document.getElementById("storyline").innerHTML = "You decide to stay with your previous case, thinking there is no connection between the shooters. You go to Detective Joe to ask him for any evidence he has. He gets upset since you didn't get any evidence before from the crime scene or asked for help right away. He says that he doesn't want to help you on this case, and that he has many other cases to work on.";
 	document.getElementById("remind").innerHTML = "What do you do next?";
-	document.getElementById("buttons").innerHTML = "<input type='button' onClick='next(16)' value='Apologize for your lack of evidence'><input type='button' onClick='next(17)' value='Argue back to defend yourself'>";
-	document.getElementById("image").innerHTML="<img src='images/anger.jpg' alt='test' width='455'>";
+	document.getElementById("buttons").innerHTML = "<input type='button' onClick='next(17)' value='Apologize for your lack of evidence'><input type='button' onClick='next(16)' value='Argue back to defend yourself'>";
+	document.getElementById("image").innerHTML="<img src='images/anger.jpg' alt='anger' width='455'>";
 	break;
 case 16:
 	alert("Sorry, " + name + "ending isn't the best.... Try again?");
@@ -121,7 +119,7 @@ case 16:
 	document.getElementById("storyline").innerHTML = "You get angry with Dectective Joe and say that you were very tired, and could have made more mistakes since you had no idea what to do. You also say that this is your first big case, so he has no right to judge you. Dectective Joe gets annoyed and starts yelling at you/ Even worse, he goes to Dectective patty's office and reports how slow your progress is. She comes to see you and you try to defend yourself. However, Patty agrees with Joe and says that you don't have the potential to be a good dectective, and that your first priority should be your job. She also explains that you shouldn't have yelled at your colleagues, so she fires you.";
 	document.getElementById("remind").innerHTML = " ";
 	document.getElementById("buttons").innerHTML = "<input type='button' onClick='next(1)' value='Restart the Story'> ";
-	document.getElementById("image").innerHTML=" ";
+	document.getElementById("image").innerHTML="<img src='images/fired.jpg' alt='fired' width='455'>";
 	break;
 case 17:
 	alert(name + " didn't have a successful career... Let's do it again!");
@@ -129,14 +127,14 @@ case 17:
 	document.getElementById("storyline").innerHTML = "You apologize to Joe, asking him if he could still help you despite your carelessness. He forgives you. Patty then comes in and asks how much progress you made on the case. You sigh and tell her your situation. She get upset and says that you don't have the potential to work on such an important case. She lowers your position to dectective in training.";
 	document.getElementById("remind").innerHTML = " ";
 	document.getElementById("buttons").innerHTML = "<input type='button' onClick='next(1)' value='Restart the Story'>";
-	document.getElementById("image").innerHTML=" ";
+	document.getElementById("image").innerHTML="<img src='images/happy.jpg' alt='happy' width='455'>";
 	break;
 case 18:
 	document.getElementById("title").innerHTML = "Final Connection";
 	document.getElementById("storyline").innerHTML = "You think there is a connection between the shooters, so you decide to take on the new case. You tell Dectective Patty about what you had seen last night, telling her that there could be a connection between the shootings. She agrees and is happy that you told her right away. She wants you to investigate the footage and check for files.";
 	document.getElementById("remind").innerHTML = "Let's start investigating!";
 	document.getElementById("buttons").innerHTML = "<input type='button' onClick='next(19)' value='Look at the footage'>";
-	document.getElementById("image").innerHTML="<img src='images/happy.jpg' alt='test' width='455'> ";
+	document.getElementById("image").innerHTML="<img src='images/sad.jpg' alt='sad' width='455'> ";
 	break;
 case 19:
 	document.getElementById("title").innerHTML = "Footage Revealed";
@@ -158,6 +156,7 @@ function save_cookies(caseStr) {
 	
 	// Store your cookies like so:
 	document.cookie = "case=" + caseStr + "; " + expires;
+	//document.cookie = "name=" + n + "; " + expires;
 	//alert(document.cookie);
 }
 
@@ -192,5 +191,8 @@ function load_cookies() {
 			// Pass that value to changeDisplay():
 			next(value);
 		}
+		/*if (key === "name") {
+			name= value;
+		}*/
 	}	
 }
